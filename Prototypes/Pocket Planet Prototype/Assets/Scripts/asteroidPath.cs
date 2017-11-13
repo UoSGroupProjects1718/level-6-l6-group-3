42f3faac36f5cfca8ffa85e51c9f5e6b865b075b
Ryan Jake Manthorp <ryanmanthorp@hotmail.co.uk>
2017-11-13 17:13:22 +0000

    Fixed gravity field problems

---
+++ Prototypes/Pocket Planet Prototype/Assets/Scripts/asteroidPath.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class asteroidPath : MonoBehaviour {
+	public Transform target;
+	public float speed;
+	public float rotSpeed;
+	void Update() 
+	{
+		if (gameObject.GetComponent<PlayerGravityBody> ().inGravityField == false) {
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+			transform.Rotate (Vector3.right * rotSpeed *Time.deltaTime);
+			transform.Rotate (Vector3.up * rotSpeed *Time.deltaTime);
+		}
+	}
+
+}
