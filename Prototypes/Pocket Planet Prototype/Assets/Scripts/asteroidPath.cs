1098d5d8cce428d63c7240b437612c5b36cc3ec9
Ryan Jake Manthorp <ryanmanthorp@hotmail.co.uk>
2017-11-14 14:55:10 +0000

    Added ability to throw rocks

--- Prototypes/Pocket Planet Prototype/Assets/Scripts/asteroidPath.cs
+++ Prototypes/Pocket Planet Prototype/Assets/Scripts/asteroidPath.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class asteroidPath : MonoBehaviour {
 	public Transform target;
 	public float speed;
 	public float rotSpeed;
+	public bool onPath = true;
 	void Update() 
 	{
-		if (gameObject.GetComponent<PlayerGravityBody> ().inGravityField == false) {
+		if (gameObject.GetComponent<PlayerGravityBody> ().inGravityField == false && onPath == true ) {
 			float step = speed * Time.deltaTime;
 			transform.position = Vector3.MoveTowards (transform.position, target.position, step);
 			transform.Rotate (Vector3.right * rotSpeed *Time.deltaTime);
 			transform.Rotate (Vector3.up * rotSpeed *Time.deltaTime);
 		}
+	}
+
+	void OnBecameInvisible() {
+		Destroy(gameObject);
 	}
 
 }
--- Prototypes/Pocket Planet Prototype/Assets/Scripts/moveObjects.cs
+++ Prototypes/Pocket Planet Prototype/Assets/Scripts/moveObjects.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveObjects : MonoBehaviour {
+public class moveObjects : MonoBehaviour  {
+
+	private Camera theCamera;
+	public float flingValue;
 
 	private Vector3 screenPoint;
 	private Vector3 offset;
 
+	void Start ()
+	{
+		theCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+	}
+		
 	void OnMouseDown(){
 		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
 		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 	}
 
-	void OnMouseDrag(){
-		Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+	void OnMouseDrag()
+	{
+		Vector3 cursorScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint (cursorScreenPoint) + offset;
 		transform.position = cursorPosition;
 	}
+
+	void OnMouseUp()
+	{
+		GetComponent<Rigidbody>().AddForce(theCamera.transform.right * Input.GetAxis("Mouse X") * flingValue, ForceMode.Impulse);
+		GetComponent<Rigidbody>().AddForce(theCamera.transform.up * Input.GetAxis("Mouse Y") * flingValue, ForceMode.Impulse);
+		GetComponent<Renderer>().material.color = Color.cyan;
+		gameObject.GetComponent<asteroidPath> ().onPath = false;
+	}
+
 }
