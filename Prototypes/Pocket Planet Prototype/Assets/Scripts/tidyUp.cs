eb3b845bf0b51a7f3463e7a0a81bce411007b780
Ryan Jake Manthorp <ryanmanthorp@hotmail.co.uk>
2017-11-28 21:58:04 +0000

    Added mobile input and UI things

---
+++ Prototypes/Pocket Planet Prototype/Assets/Scripts/tidyUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tidyUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.tag == "rock") 
+		{
+
+			col.gameObject.transform.position = new Vector3 (1400, 450, -136);
+			col.gameObject.GetComponent<PlayerGravityBody> ().inGravityField = false;
+			col.gameObject.GetComponent<asteroidPath> ().onPath = true;
+
+
+
+		}
+	}
+
+}
