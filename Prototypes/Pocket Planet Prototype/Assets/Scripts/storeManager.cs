af42a20eef7bf9b1539917bccdd4a4c8a3e5c5cd
Daniel Pokladek <danielpokladek@gmail.com>
2018-04-30 16:27:06 +0100

    Added store items

--- Prototypes/Pocket Planet Prototype/Assets/Scripts/storeManager.cs
+++ Prototypes/Pocket Planet Prototype/Assets/Scripts/storeManager.cs
@@ -13,6 +13,8 @@ public class storeManager : MonoBehaviour {
 	public Transform PyramidItem;
 	public Transform shipItem;
 
+	public Button Gatcha;
+
 	public Button treeButton;
 	public Button PyramidButton;
 	public Button shipButton;
@@ -38,7 +40,8 @@ public class storeManager : MonoBehaviour {
 		Button buyShip = shipButton.GetComponent<Button> ();
 		buyShip.onClick.AddListener (purchaseShip);
 
-
+		Button gatcha = Gatcha.GetComponent<Button> ();
+		gatcha.onClick.AddListener (ActivateGatcha);
 
 	}
 
@@ -81,10 +84,13 @@ public class storeManager : MonoBehaviour {
 			Debug.Log ("Not Enough Faith");
 		}
 		Instantiate(storeItems[0], new Vector3 (itemSpawnLocation.GetComponent<Transform> ().position.x, itemSpawnLocation.GetComponent<Transform> ().position.y, itemSpawnLocation.GetComponent<Transform> ().position.z), Quaternion.Euler (-90, 0, -180));
-
-
 	}
 
+
+	void ActivateGatcha()
+	{
+		Instantiate(storeItems[Random.Range(0,20)], new Vector3 (itemSpawnLocation.GetComponent<Transform> ().position.x, itemSpawnLocation.GetComponent<Transform> ().position.y, itemSpawnLocation.GetComponent<Transform> ().position.z), Quaternion.Euler (-90, 0, -180));
+	}
 
 	// Update is called once per frame
 	void Update ()
